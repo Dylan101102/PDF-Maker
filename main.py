347747ad1c883a0d0fb71bdcad33333c582a7560
Dylan101102 <136282326+Dylan101102@users.noreply.github.com>
2025-04-11 20:46:19 -0400

    Add more pages for each topic

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ pdf = FPDF(orientation='P', unit='mm', format='A4') # Will be how each PDF page 
 # print(dataframe)
 
 for index, row in dataframe.iterrows():
-    pdf.add_page()
-    pdf.set_font(family='Times', style='B', size=24)
-    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
+    for page in range(row['Pages']):
+        pdf.add_page()
+        pdf.set_font(family='Times', style='B', size=24)
+        pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
+        pdf.line(10, 21, 200, 21)
 pdf.output('output.pdf')
