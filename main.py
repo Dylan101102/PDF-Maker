754d58f43e56dc23b13d4bc67274346a8f8b8b14
Dylan101102 <136282326+Dylan101102@users.noreply.github.com>
2025-04-11 20:21:35 -0400

    Add gitignore and create initial pdf

--- main.py
+++ main.py
@@ -4,18 +4,11 @@ import pandas as pd
 dataframe = pd.read_csv("topics(2).csv")
 pdf = FPDF(orientation='P', unit='mm', format='A4') # Will be how each PDF page looks.
 
-print(dataframe["Topic"], dataframe["Pages"])
+# print(dataframe)
 
-
-"""
-pdf = FPDF(orientation='P', unit='mm', format='A4')
-
-pdf.add_page()
-
-pdf.set_font(family='Times', style='B', size=12)
-pdf.cell(w=0, h=12, txt='Hello There!', border=1, align='L', ln=1)
-pdf.cell(w=0, h=12, txt='Hi There', border=1, align='L', ln=1)
-
-
+for index, row in dataframe.iterrows():
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=24)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
+    pdf.line(10, 21, 200, 21)
 pdf.output('output.pdf')
-"""
